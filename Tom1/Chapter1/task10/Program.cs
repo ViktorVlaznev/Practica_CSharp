d9f9cdd91e8d06ddf376eb2722ad11f0a7eeeabf
Vlaznev Victor <v-vlaznev@mail.ru>
2022-10-07 12:34:30 +0300

    Add task10 in Chapter1 in Tom1

---
+++ Tom1/Chapter1/task10/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, в которой пользователь вводит два числа,а программой вычисляется и отображается
+// сумма и разность этих чисел. Предложите варианты программы с использованием консольного ввода/вывода
+// данных и ввода и вывода данных с помощью диалоговых окон.
+
+using Microsoft.VisualBasic;
+
+int num1 = int.Parse(Interaction.InputBox("Введите первое число:", "Первое число"));
+int num2 = int.Parse(Interaction.InputBox("Введите второе число:", "Второе число"));
+MessageBox.Show(
+    $"Сумма: {num1} + {num2} = {num1 + num2}, разность: {num1} - {num2} = {num1 - num2}!",
+    "Сумма"
+);
+
+Console.Write("Введите первое число: ");
+num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+num2 = int.Parse(Console.ReadLine());
+Console.WriteLine(
+    $"Сумма: {num1} + {num2} = {num1 + num2}, разность: {num1} - {num2} = {num1 - num2}!"
+);
