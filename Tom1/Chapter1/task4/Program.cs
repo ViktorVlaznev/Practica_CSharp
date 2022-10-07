87cf3a41d5031e03451dfaf5707a66f54f825e29
Vlaznev Victor <v-vlaznev@mail.ru>
2022-10-07 11:13:27 +0300

    Add task4 in Chapter1 in Tom1

---
+++ Tom1/Chapter1/task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, в которой пользователю предлагается ввести название месяца и количество дней
+// в этом месяце. Программа выводит сообщение о том. что соответствующий месяц содержит указанное количество
+// дней. Предложите версии программы для ввода/вывода данных через консоль и с помощью диалоговых окон.
+using Microsoft.VisualBasic;
+
+Console.Write("Введите название месяца: ");
+string month = Console.ReadLine();
+Console.Write("Введите количество дней в этом месяце: ");
+int daysMonth = int.Parse(Console.ReadLine());
+string txt = "В " + month + " " + daysMonth + " дней.";
+Console.WriteLine(txt);
+
+month = Interaction.InputBox("Введите название месяца:", "Месяц");
+daysMonth = int.Parse(Interaction.InputBox("ведите количество дней в этом месяце:", "Месяц"));
+txt = "В " + month + " " + daysMonth + " дней.";
+MessageBox.Show(txt, "Месяц");
