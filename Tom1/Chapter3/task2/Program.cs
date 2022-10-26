b286244c37533b26f5b01fe97ad80bbb1b51095b
Vlaznev Victor <v-vlaznev@mail.ru>
2022-10-26 15:55:20 +0300

    Add task2 in Chapter3 in Tom1

---
+++ Tom1/Chapter3/task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, в которой пользователь последовательно вводит два
+// целых числа. Программа определяет, какое из чисел больше или они равны,
+// и выводит сообщение в диалоговом окне. Используейте обработку исключений.
+
+using Microsoft.VisualBasic;
+
+try
+{
+int numberA = int.Parse(Interaction.InputBox("Введите целое число a:", "Какое число больше?"));
+int numberB= int.Parse(Interaction.InputBox("Введите целое число b:", "Какое число больше?"));
+
+if (numberA > numberB)
+    {
+        MessageBox.Show($"{numberA} > {numberB}", "Какое число больше?");
+    } else if (numberA < numberB) 
+    {
+        MessageBox.Show($"{numberB} > {numberA}", "Какое число больше?");
+    } else {MessageBox.Show($"{numberB} = {numberA}", "Какое число больше?");}
+}
+catch
+{
+    MessageBox.Show("Введено не число", "Проверка числа на деление");
+}
