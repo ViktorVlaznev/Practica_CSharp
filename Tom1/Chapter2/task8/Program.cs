043dbbb1e39b7bf0a1efe9db2fee1e6bfc0738a9
Vlaznev Victor <v-vlaznev@mail.ru>
2022-10-26 13:50:06 +0300

    Add task8 in Chapter2 in Tom1

---
+++ Tom1/Chapter2/task8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, в которой для введенного пользователем числа
+// в бинарном представлении третий бит устанавливает равным единице.
+
+using Microsoft.VisualBasic;
+
+string ChangeThirdCharinBinar(int num)
+{   
+    num |= 1 << 2;
+    string binar = Convert.ToString(num, 2);
+    
+    return binar;
+}
+
+int number = int.Parse(Interaction.InputBox("Введите целое число:", "Двоичная система"));
+
+MessageBox.Show(ChangeThirdCharinBinar(number), "Двоичная система");
