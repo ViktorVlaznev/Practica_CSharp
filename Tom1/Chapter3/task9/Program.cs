857a031efaa1b11350bd4c49a2852c27f59b3045
Vlaznev Victor <v-vlaznev@mail.ru>
2022-11-04 12:53:30 +0300

    Add task9 in Chapter3 in Tom1

---
+++ Tom1/Chapter3/task9/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, в которой пользователем вводится два целых числа.
+// Программа выводит все целые числа - начиная с наименьшего (из двух введенных чисел)
+// и заканчивая наибольшим(из двух введенных чисел). Предложите разные версии программы
+// (с использованием разных операторов цикла), а также механизм обработки исключений для этой программы.
+
+using Microsoft.VisualBasic;
+
+try
+{
+    int numberA = int.Parse(Interaction.InputBox("Введите число a: ", "Ряд чисел"));
+    int numberB = int.Parse(Interaction.InputBox("Введите число b: ", "Ряд чисел"));
+    int min = (numberA > numberB) ? numberB : numberA;
+    int max = (numberA > numberB) ? numberA : numberB;
+    string strNumber = "";
+    while (min <= max)
+    {
+        strNumber += $"{min++} ";
+    }
+    MessageBox.Show(strNumber, "Ряд чисел");
+}
+catch
+{
+    MessageBox.Show("Введено не число", "Ряд чисел");
+}
