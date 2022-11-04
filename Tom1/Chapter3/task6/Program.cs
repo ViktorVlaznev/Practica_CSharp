7af6af1db27f58436481223daeece7a5918ecfa9
Vlaznev Victor <v-vlaznev@mail.ru>
2022-11-04 10:04:12 +0300

    Add task6 in Chapter3 in Tom1

---
+++ Tom1/Chapter3/task6/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, в которой вычисляется сумма нечетных чисел.
+// Для проверки результата воспользуйтес тем, что 2+4+6+...+2n= n(n+1).
+// Предложите версии программы, использующие разные операторы циклы.
+
+using Microsoft.VisualBasic;
+
+/// <summary>
+/// функция возвращает сумму арифметической прогрессии из целых чисел
+/// </summary>
+/// <param name="minValue">первоначальный элемент</param>
+/// <param name="maxValue">конечный элемент</param>
+/// <param name="step">шаг</param>
+/// <returns>сумма арифметической прогрессии</returns>
+int GetSumArithmeticProgression(int minValue, int maxValue, int step)
+{
+    return (minValue + maxValue) * (1 + (maxValue - minValue) / step) / 2;
+}
+
+try
+{
+    int number = int.Parse(Interaction.InputBox("Введите натуральное число:", "Сумма числа"));
+    int sum = 0;
+    number = number % 2 == 0 ? number - 1 : number;
+    MessageBox.Show($"Итог = {GetSumArithmeticProgression(1, number, 2)}", "Сумма чисел");
+    while (number > 0)
+    {
+        sum += number;
+        number -= 2;
+    }
+    MessageBox.Show($"Итог = {sum}", "Сумма чисел");
+}
+catch
+{
+    MessageBox.Show("Введено не число", "Сумма чисел");
+}
